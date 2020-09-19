dc856c60b7ecce1fa1f424f887afecc4be5e965f
BlazeATG <ubietaalex@gmail.com>
2020-09-19 00:17:56 -0500

    One axis is turned on at a time for snake-like movement

--- Pet the Dog!/Assets/Scripts/DUMMYmovement.cs
+++ Pet the Dog!/Assets/Scripts/DUMMYmovement.cs
@@ -13,48 +13,38 @@ public class DUMMYmovement : MonoBehaviour
         rigidbody = this.GetComponent<Rigidbody2D>();
     }
 
-    bool moveRight = false;
-    //bool moveLeft = false;
-    bool moveUp = false;
-    //bool moveDown = false;
+    bool moveVert = false;
+    bool moveHori = false;
     float storeX = 0;
     float storeY = 0;
     void Update()
     {
-        // movement.x = Input.GetAxisRaw("Horizontal");
-        // movement.y = Input.GetAxisRaw("Vertical");
 
         if (Input.GetKey(KeyCode.D))
         {
-            moveRight = true;
-            //moveLeft = false;
-            moveUp = false;
-            //oveDown = false;
+            moveVert = true;
+            moveHori = false;
             storeX = Input.GetAxisRaw("Horizontal");
             movement.y = 0;
         }
         
         if (Input.GetKey(KeyCode.A))
         {
-            moveRight = true;
-            //moveLeft = false;
-            moveUp = false;
-            //oveDown = false;
+            moveVert = true;
+            moveHori = false;
             storeX = Input.GetAxisRaw("Horizontal");
             movement.y = 0;
         }
         
-        if (moveRight)
+        if (moveVert)
         {
             movement.x = storeX;
         }
         
         if (Input.GetKey(KeyCode.W))
         {
-            moveRight = false;
-            //moveLeft = false;
-            moveUp = true;
-            //oveDown = false;
+            moveVert = false;
+            moveHori = true;
             storeY = Input.GetAxisRaw("Vertical");
             movement.x = 0;
             
@@ -62,15 +52,13 @@ public class DUMMYmovement : MonoBehaviour
         
         if (Input.GetKey(KeyCode.S))
         {
-            moveRight = false;
-            //moveLeft = false;
-            moveUp = true;
-            //oveDown = false;
+            moveVert = false;
+            moveHori = true;
             storeY = Input.GetAxisRaw("Vertical");
             movement.x = 0;
         }
 
-        if (moveUp)
+        if (moveHori)
         {
             movement.y = storeY;
         }
