20d5dc3a35b62e913cc75c284abcf35f1672358a
loremonger <47060398+loremonger@users.noreply.github.com>
2020-09-19 11:57:10 -0500

    Simple Title Screen, Win & Lose screens
    need to populate them with art

---
+++ Pet the Dog!/Assets/Scripts/Scenes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public GameObject titleScreen;
+    public GameObject instructionsScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LeaveTitleScreen()
+    {
+        titleScreen.SetActive(false);
+        instructionsScreen.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
