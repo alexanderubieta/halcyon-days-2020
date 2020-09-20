04f954bcc2121688f677539a83963f8bd136b01d
loremonger <47060398+loremonger@users.noreply.github.com>
2020-09-19 20:42:19 -0500

    Dogs added

--- Pet the Dog!/Assets/Scripts/Hazard.cs
+++ Pet the Dog!/Assets/Scripts/Hazard.cs
@@ -8,11 +8,21 @@ public class Hazard : MonoBehaviour
     public AudioSource damagedSFX;
     private HazardManager hm;
     private bool isColliding = false;
+    public GameObject trueForm;
+    private Animator dogANIM;
 
     void Awake()
     {
         myCamera = GameObject.Find("Main Camera");
         hm = GameObject.Find("HazardMGR").GetComponent<HazardManager>();
+        trueForm = gameObject.transform.GetChild(1).gameObject;
+        dogANIM = gameObject.GetComponent<Animator>();
+        dogANIM.SetInteger("dogNum", Random.Range(1, 10));
+    }
+
+    void OnEnable()
+    {
+        dogANIM.SetInteger("dogNum", Random.Range(1, 10));
     }
 
     void Update()
@@ -27,6 +37,8 @@ public class Hazard : MonoBehaviour
             damagedSFX.Play();
             other.GetComponent<PlayerHealth>().arms -= 1; // Remove an arm from the Player
             myCamera.GetComponent<CameraShake>().TriggerShake();
+            trueForm.SetActive(true);
+            trueForm.GetComponent<Transform>().parent = null;
 
             // The following code ensures that BackBatch only runs once.
             if (isColliding) return;
