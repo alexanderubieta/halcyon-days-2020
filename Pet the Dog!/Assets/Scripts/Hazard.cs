ee9f1a0b347137c21ce5cf230e9ddea65d1c09af
loremonger <47060398+loremonger@users.noreply.github.com>
2020-09-19 23:44:08 -0500

    Music, house tiles, tweaked gameplay

--- Pet the Dog!/Assets/Scripts/Hazard.cs
+++ Pet the Dog!/Assets/Scripts/Hazard.cs
@@ -10,14 +10,22 @@ public class Hazard : MonoBehaviour
     private bool isColliding = false;
     public GameObject trueForm;
     private Animator dogANIM;
+    private Sprite mySprite;
+    private TextMaker tm;
+    public string truth;
+
 
     void Awake()
     {
         myCamera = GameObject.Find("Main Camera");
         hm = GameObject.Find("HazardMGR").GetComponent<HazardManager>();
         trueForm = gameObject.transform.GetChild(1).gameObject;
+        trueForm.SetActive(false);
         dogANIM = gameObject.GetComponent<Animator>();
         dogANIM.SetInteger("dogNum", Random.Range(1, 10));
+        mySprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        tm = GameObject.Find("TextMaker").GetComponent<TextMaker>();
+        
     }
 
     void OnEnable()
@@ -25,9 +33,16 @@ public class Hazard : MonoBehaviour
         dogANIM.SetInteger("dogNum", Random.Range(1, 10));
     }
 
+    void OnDisable()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = mySprite;
+        gameObject.GetComponent<Transform>().localScale = new Vector3(13, 13, 13);
+    }
+
     void Update()
     {
         isColliding = false;
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -37,15 +52,16 @@ public class Hazard : MonoBehaviour
             damagedSFX.Play();
             other.GetComponent<PlayerHealth>().arms -= 1; // Remove an arm from the Player
             myCamera.GetComponent<CameraShake>().TriggerShake();
+
             trueForm.SetActive(true);
-            trueForm.GetComponent<Transform>().parent = null;
+            trueForm.GetComponent<Transform>().position = gameObject.GetComponent<Transform>().position;
+            tm.phrase = truth;
+            tm.Exclaim();
 
             // The following code ensures that BackBatch only runs once.
             if (isColliding) return;
             isColliding = true;
             hm.BackBatch();
-
-            
         }
     }
 }
