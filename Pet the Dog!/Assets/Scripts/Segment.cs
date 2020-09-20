5924fe9145e0c102e741554e6aea10e6b155eccd
loremonger <47060398+loremonger@users.noreply.github.com>
2020-09-20 02:26:57 -0500

    Segmented arm sprites

---
+++ Pet the Dog!/Assets/Scripts/Segment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment : MonoBehaviour
+{
+    public int myDirection;
+    public Animator segmentANIM;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myDirection = Random.Range(0, 3);
+        player = GameObject.Find("Player");
+        gameObject.GetComponent<Animator>().runtimeAnimatorController = player.transform.GetChild(0).GetComponent<Animator>().runtimeAnimatorController;
+        segmentANIM.SetInteger("direction", Random.Range(0, 3));
+        gameObject.transform.localScale = new Vector3(20f, 20f, 20f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
